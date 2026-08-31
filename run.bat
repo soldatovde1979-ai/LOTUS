da82282d9da897e31e42e27c48010cf21a70f3cd
sDimpson <soldatov.de1979@gmail.com>
2026-08-31 10:52:59 +0300

    1

--- run.bat
+++ run.bat
@@ -2,16 +2,18 @@
 chcp 65001 >nul
 title Restart notes_gemini
 
+
 echo Stopping old process...
 cd c:\Projects\LOTUS
 powershell -Command "Get-Process py -ErrorAction SilentlyContinue | Where-Object { $_.MainWindowTitle -like '*notes_gemini*' -or $_.Path -like '*python*' } | Stop-Process -Force -ErrorAction SilentlyContinue"
 
+
 echo Starting notes_gemini...
 start "" powershell -NoExit -Command "py notes_gemini.py"
+
 
 echo Opening browser...
 timeout /t 2 /nobreak >nul
 start "" "http://127.0.0.1:8765"
 
 echo Done!
-pause
